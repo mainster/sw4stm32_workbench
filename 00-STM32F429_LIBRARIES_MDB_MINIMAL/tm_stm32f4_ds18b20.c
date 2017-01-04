7831f6be0c6080f48b9596864ab920b706795c4e
Manuel Del Basso <manuel.delbasso@gmail.com>
2017-01-04 05:37:43 +0100

    library parts added

---
+++ 00-STM32F429_LIBRARIES_MDB_MINIMAL/tm_stm32f4_ds18b20.c
@@ -0,0 +1,354 @@
+/**	
+ * |----------------------------------------------------------------------
+ * | Copyright (C) Tilen Majerle, 2014
+ * | 
+ * | This program is free software: you can redistribute it and/or modify
+ * | it under the terms of the GNU General Public License as published by
+ * | the Free Software Foundation, either version 3 of the License, or
+ * | any later version.
+ * |  
+ * | This program is distributed in the hope that it will be useful,
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * | GNU General Public License for more details.
+ * | 
+ * | You should have received a copy of the GNU General Public License
+ * | along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * |----------------------------------------------------------------------
+ */
+#include "tm_stm32f4_ds18b20.h"
+
+uint8_t TM_DS18B20_Start(uint8_t *ROM) {
+	if (!TM_DS18B20_Is(ROM)) {
+		return 0;
+	}
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Start temperature conversion
+	TM_OneWire_WriteByte(TM_DS18B20_CMD_CONVERTTEMP);
+	
+	return 1;
+}
+
+void TM_DS18B20_StartAll(void) {
+	//Reset pulse
+	TM_OneWire_Reset();
+	//Skip rom
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_SKIPROM);
+	//Start conversion on all connected devices
+	TM_OneWire_WriteByte(TM_DS18B20_CMD_CONVERTTEMP);
+}
+
+uint8_t TM_DS18B20_Read(uint8_t *ROM, float *destination) {
+	uint16_t temperature;
+	uint8_t resolution;
+	int8_t digit, minus = 0;
+	float decimal;
+	
+	if (!TM_DS18B20_Is(ROM)) {
+		return 0;
+	}
+	
+	//Check if line is released, if it is, then conversion is complete
+	if (!TM_OneWire_ReadBit()) {
+		return 0; //Conversion is not finished yet
+	}
+
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Read scratchpad command by onewire protocol
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_RSCRATCHPAD);
+	
+	//First two bytes of scratchpad are temperature values
+	temperature = TM_OneWire_ReadByte() | (TM_OneWire_ReadByte() << 8);
+
+	//Reset line
+	TM_OneWire_Reset();
+	
+	if (((temperature >> 15)) == 1) {
+		//Two's complement, temperature is negative
+		temperature = ~temperature + 1;
+		minus = 1;
+	}
+
+	//Get sensor resolution
+	resolution = TM_DS18B20_GetResolution(ROM);
+	
+	//Store temperature integer digits and decimal digits
+	digit = temperature >> 4;
+	digit |= ((temperature >> 8) & 0x7) << 4;
+	//Store decimal digits
+	switch (resolution) {
+		case 9: {
+			decimal = (temperature >> 3) & 0x01;
+			decimal *= (float)TM_DS18B20_DECIMAL_STEPS_9BIT;
+		} break;
+		case 10: {
+			decimal = (temperature >> 2) & 0x03;
+			decimal *= (float)TM_DS18B20_DECIMAL_STEPS_10BIT;
+		} break;
+		case 11: {
+			decimal = (temperature >> 1) & 0x07;
+			decimal *= (float)TM_DS18B20_DECIMAL_STEPS_11BIT;
+		} break;
+		case 12: {
+			decimal = temperature & 0x0F;
+			decimal *= (float)TM_DS18B20_DECIMAL_STEPS_12BIT;
+		} break;
+		default: {
+			decimal = 0xFF;
+			digit = 0;
+		}
+	}
+	
+	decimal = digit + decimal;
+	if (minus) {
+		decimal = 0 - decimal;
+	}
+	
+	*destination = decimal;
+	
+	return 1;
+}
+
+uint8_t TM_DS18B20_GetResolution(uint8_t *ROM) {
+	uint8_t conf;
+	if (!TM_DS18B20_Is(ROM)) {
+		return 0;
+	}
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Read scratchpad command by onewire protocol
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_RSCRATCHPAD);
+	
+	TM_OneWire_ReadByte();
+	TM_OneWire_ReadByte();
+	TM_OneWire_ReadByte();
+	TM_OneWire_ReadByte();
+	//5th byte of scratchpad is configuration register
+	conf = TM_OneWire_ReadByte();
+	
+	return ((conf & 0x60) >> 5) + 9;
+}
+
+uint8_t TM_DS18B20_SetResolution(uint8_t *ROM, TM_DS18B20_Resolution_t resolution) {
+	uint8_t th, tl, conf;
+	if (!TM_DS18B20_Is(ROM)) {
+		return 0;
+	}
+	
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Read scratchpad command by onewire protocol
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_RSCRATCHPAD);
+	
+	TM_OneWire_ReadByte();
+	TM_OneWire_ReadByte();
+	
+	th = TM_OneWire_ReadByte();
+	tl = TM_OneWire_ReadByte();
+	conf = TM_OneWire_ReadByte();
+	
+	if (resolution == TM_DS18B20_Resolution_9bits) {
+		conf &= ~(1 << TM_DS18B20_RESOLUTION_R1);
+		conf &= ~(1 << TM_DS18B20_RESOLUTION_R0);
+	} else if (resolution == TM_DS18B20_Resolution_10bits) {
+		conf &= ~(1 << TM_DS18B20_RESOLUTION_R1);
+		conf |= 1 << TM_DS18B20_RESOLUTION_R0;
+	} else if (resolution == TM_DS18B20_Resolution_11bits) {
+		conf |= 1 << TM_DS18B20_RESOLUTION_R1;
+		conf &= ~(1 << TM_DS18B20_RESOLUTION_R0);
+	} else if (resolution == TM_DS18B20_Resolution_12bits) {
+		conf |= 1 << TM_DS18B20_RESOLUTION_R1;
+		conf |= 1 << TM_DS18B20_RESOLUTION_R0;
+	}
+	
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Write scratchpad command by onewire protocol, only th, tl and conf register can be written
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_WSCRATCHPAD);
+	
+	//Write bytes
+	TM_OneWire_WriteByte(th);
+	TM_OneWire_WriteByte(tl);
+	TM_OneWire_WriteByte(conf);
+	
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Copy scratchpad to EEPROM of DS18B20
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_CPYSCRATCHPAD);
+	
+	return 1;
+}
+
+uint8_t TM_DS18B20_Is(uint8_t *ROM) {
+	//Checks if first byte is equal to DS18B20's family code (0x28)
+	if (*ROM == TM_DS18B20_FAMILY_CODE) {
+		return 1;
+	}
+	return 0;
+}
+
+uint8_t TM_DS18B20_SetAlarmLowTemperature(uint8_t *ROM, int8_t temp) {
+	uint8_t tl, th, conf;
+	if (!TM_DS18B20_Is(ROM)) {
+		return 0;
+	}
+	if (temp > 125) {
+		temp = 125;
+	} 
+	if (temp < -55) {
+		temp = -55;
+	}
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Read scratchpad command by onewire protocol
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_RSCRATCHPAD);
+	
+	TM_OneWire_ReadByte();
+	TM_OneWire_ReadByte();
+	
+	th = TM_OneWire_ReadByte();
+	tl = TM_OneWire_ReadByte();
+	conf = TM_OneWire_ReadByte();
+	
+	tl = (uint8_t)temp; 
+
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Write scratchpad command by onewire protocol, only th, tl and conf register can be written
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_WSCRATCHPAD);
+	
+	//Write bytes
+	TM_OneWire_WriteByte(th);
+	TM_OneWire_WriteByte(tl);
+	TM_OneWire_WriteByte(conf);
+	
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Copy scratchpad to EEPROM of DS18B20
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_CPYSCRATCHPAD);
+	
+	return 1;
+}
+
+uint8_t TM_DS18B20_SetAlarmHighTemperature(uint8_t *ROM, int8_t temp) {
+	uint8_t tl, th, conf;
+	if (!TM_DS18B20_Is(ROM)) {
+		return 0;
+	}
+	if (temp > 125) {
+		temp = 125;
+	} 
+	if (temp < -55) {
+		temp = -55;
+	}
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Read scratchpad command by onewire protocol
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_RSCRATCHPAD);
+	
+	TM_OneWire_ReadByte();
+	TM_OneWire_ReadByte();
+	
+	th = TM_OneWire_ReadByte();
+	tl = TM_OneWire_ReadByte();
+	conf = TM_OneWire_ReadByte();
+	
+	th = (uint8_t)temp; 
+
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Write scratchpad command by onewire protocol, only th, tl and conf register can be written
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_WSCRATCHPAD);
+	
+	//Write bytes
+	TM_OneWire_WriteByte(th);
+	TM_OneWire_WriteByte(tl);
+	TM_OneWire_WriteByte(conf);
+	
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Copy scratchpad to EEPROM of DS18B20
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_CPYSCRATCHPAD);
+	
+	return 1;
+}
+
+uint8_t TM_DS18B20_DisableAlarmTemperature(uint8_t *ROM) {
+	uint8_t tl, th, conf;
+	if (!TM_DS18B20_Is(ROM)) {
+		return 0;
+	}
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Read scratchpad command by onewire protocol
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_RSCRATCHPAD);
+	
+	TM_OneWire_ReadByte();
+	TM_OneWire_ReadByte();
+	
+	th = TM_OneWire_ReadByte();
+	tl = TM_OneWire_ReadByte();
+	conf = TM_OneWire_ReadByte();
+	
+	th = 125;
+	tl = (uint8_t)-55;
+
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Write scratchpad command by onewire protocol, only th, tl and conf register can be written
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_WSCRATCHPAD);
+	
+	//Write bytes
+	TM_OneWire_WriteByte(th);
+	TM_OneWire_WriteByte(tl);
+	TM_OneWire_WriteByte(conf);
+	
+	//Reset line
+	TM_OneWire_Reset();
+	//Select ROM number
+	TM_OneWire_SelectWithPointer(ROM);
+	//Copy scratchpad to EEPROM of DS18B20
+	TM_OneWire_WriteByte(TM_ONEWIRE_CMD_CPYSCRATCHPAD);
+	
+	return 1;
+}
+
+uint8_t TM_DS18B20_AlarmSearch(void) {
+	return TM_OneWire_Search(TM_DS18B20_CMD_ALARMSEARCH);
+}
+
+uint8_t TM_DS18B20_AllDone(void) {
+	return TM_OneWire_ReadBit();
+}
+
+
---
+++ 00-STM32F429_LIBRARIES_MDB_MINIMAL/tm_stm32f4_onewire.h
@@ -0,0 +1,196 @@
+/**
+ *	Onewire library for STM32F4 devices
+ *
+ *	@author 	Tilen Majerle
+ *	@email		tilen@majerle.eu
+ *	@website	http://stm32f4-discovery.com
+ *	@link		http://stm32f4-discovery.com/2014/05/library-12-onewire-library-for-stm43f4xx/
+ *	@version 	v1.0
+ *	@ide		Keil uVision
+ *	@license	GNU GPL v3
+ *	
+ * |----------------------------------------------------------------------
+ * | Copyright (C) Tilen Majerle, 2014
+ * | 
+ * | This program is free software: you can redistribute it and/or modify
+ * | it under the terms of the GNU General Public License as published by
+ * | the Free Software Foundation, either version 3 of the License, or
+ * | any later version.
+ * |  
+ * | This program is distributed in the hope that it will be useful,
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * | GNU General Public License for more details.
+ * | 
+ * | You should have received a copy of the GNU General Public License
+ * | along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * |----------------------------------------------------------------------
+ *	
+ *	Onewire default pin: PD0
+ *	
+ *	If you want to use other pin, edit lines below in your defines.h file
+ * 
+ *  //Edit this files to select custom onewire pin
+ *	#define TM_ONEWIRE_RCC			RCC_AHB1Periph_GPIOD
+ *	#define TM_ONEWIRE_PORT			GPIOD
+ *	#define TM_ONEWIRE_PIN			GPIO_Pin_0
+ */
+#ifndef TM_ONEWIRE_H
+#define TM_ONEWIRE_H 100
+/**
+ * Library dependencies
+ * - STM32F4xx
+ * - STM32F4xx RCC
+ * - STM32F4xx GPIO
+ * - TM_DELAY
+ * - defines.h
+ */
+/**
+ * Includes
+ */
+#include "stm32f4xx.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+#include "tm_stm32f4_delay.h"
+#include "defines.h"
+
+//Owerwrite this in defines.h if you want to use other pin
+#ifndef TM_ONEWIRE_PIN	
+#define TM_ONEWIRE_RCC			RCC_AHB1Periph_GPIOD
+#define TM_ONEWIRE_PORT			GPIOD
+#define TM_ONEWIRE_PIN			GPIO_Pin_0
+#endif
+
+#define TM_ONEWIRE_DELAY(x)		Delay(x)
+
+#define TM_ONEWIRE_LOW			TM_ONEWIRE_PORT->BSRRH = TM_ONEWIRE_PIN;
+#define TM_ONEWIRE_HIGH			TM_ONEWIRE_PORT->BSRRL = TM_ONEWIRE_PIN;
+#define TM_ONEWIRE_INPUT		TM_OneWire_GPIO_InitDef.GPIO_Mode = GPIO_Mode_IN; GPIO_Init(TM_ONEWIRE_PORT, &TM_OneWire_GPIO_InitDef);
+#define TM_ONEWIRE_OUTPUT		TM_OneWire_GPIO_InitDef.GPIO_Mode = GPIO_Mode_OUT; GPIO_Init(TM_ONEWIRE_PORT, &TM_OneWire_GPIO_InitDef);
+
+//OneWire commands
+#define TM_ONEWIRE_CMD_RSCRATCHPAD			0xBE //Read Scratchpad
+#define TM_ONEWIRE_CMD_WSCRATCHPAD			0x4E //Write Scratchpad
+#define TM_ONEWIRE_CMD_CPYSCRATCHPAD		0x48
+#define TM_ONEWIRE_CMD_RECEEPROM			0xB8
+#define TM_ONEWIRE_CMD_RPWRSUPPLY			0xB4
+#define TM_ONEWIRE_CMD_SEARCHROM			0xF0
+#define TM_ONEWIRE_CMD_READROM				0x33
+#define TM_ONEWIRE_CMD_MATCHROM				0x55
+#define TM_ONEWIRE_CMD_SKIPROM				0xCC
+
+/**
+ * Initialize OneWire bus
+ *
+ */
+extern void TM_OneWire_Init(void);
+
+/**
+ * Reset OneWire bus
+ * 
+ * Sends reset command for OneWire
+ */
+extern uint8_t TM_OneWire_Reset(void);
+
+/**
+ * Read single bit from bus
+ *
+ * 0 or 1 is returned
+ */
+extern uint8_t TM_OneWire_ReadBit(void);
+
+/**
+ * Read byte from bus
+ *
+ * Byte is returned
+ */
+extern uint8_t TM_OneWire_ReadByte(void);
+
+/**
+ * Write single bit to bus
+ *
+ * Parameters:
+ * 	- uint8_t bit
+ * 		0 or 1, what you want to send
+ *
+ */
+extern void TM_OneWire_WriteBit(uint8_t bit);
+
+/**
+ * Write byte to bus
+ *
+ * Parameters:
+ * 	- uint8_t byte
+ * 		Byte to be sent
+ */
+extern void TM_OneWire_WriteByte(uint8_t byte);
+
+/**
+ * Search for devices
+ *
+ * Parameters:
+ * 	- uint8_t command: command type of search
+ *
+ * Returns 1 if device is found, otherwise 0
+ */
+extern uint8_t TM_OneWire_Search(uint8_t command);
+
+/**
+ * Reset search states
+ *
+ */
+extern void TM_OneWire_ResetSearch(void);
+
+/**
+ * Start search, reset states
+ *
+ * Returns 1 if any device on bus
+ */
+extern uint8_t TM_OneWire_First(void);
+
+/**
+ * Read next device
+ *
+ * Returns 1 if more devices are on bus, otherwise 0
+ */
+extern uint8_t TM_OneWire_Next(void);
+
+/**
+ * Get rom from device from search
+ *
+ * Parameters
+ * 	- uint8_t index:
+ * 		because each device has 8bytes long rom, you have to call this 8 times, to get rom bytes from 0 to 7
+ * Returns ROM byte for index (0 to 7) at current found device
+ */
+extern uint8_t TM_OneWire_GetROM(uint8_t index);
+
+/**
+ * Get all 8 bytes ROM value from device from search
+ *
+ * Parameters
+ * 	- uint8_t *firstIndex:
+ * 		Pointer to first location for first byte, other bytes are automatically incremented
+ */
+extern void TM_OneWire_GetFullROM(uint8_t *firstIndex);
+
+/**
+ * Select specific slave on bus
+ *
+ * Parameters:
+ * 	- uint8_t addr[]
+ * 		8 bytes ROM address
+ */
+extern void TM_OneWire_Select(uint8_t addr[]);
+
+/**
+ * Select specific slave on bus with pointer address
+ *
+ * Parameters:
+ * 	- uint8_t *ROM
+ * 		pointer to first byte of ROM address
+ */
+extern void TM_OneWire_SelectWithPointer(uint8_t *ROM);
+
+#endif
+
